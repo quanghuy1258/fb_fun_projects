8009072448444e40c8675d200851b713d2bf937b
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2022-04-24 10:59:21 +0700

    update code

---
+++ personal_chat_bot/node/joke_with_typing_indicator.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const login = require("facebook-chat-api");
+
+const credential = {appState: JSON.parse(fs.readFileSync('appstate.json', 'utf-8'))}
+
+login(credential, (err, api) => {
+    if(err) return console.error(err);
+
+    var someoneID = "000000000000000";
+
+    console.log(someoneID);
+    async function loop() {
+      while (true) {
+        api.sendTypingIndicator(someoneID);
+        console.log(Date());
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      }
+    }
+    loop();
+});
